6672af2fedb26c5f15ca16c40301a99f3956d930
arnavgupta <arnavg.gupta@mail.utoronto.ca>
2021-05-15 21:06:26 +0530

    Setup Server Side Render ssr

--- r_frontend/src/generated/graphql.tsx
+++ r_frontend/src/generated/graphql.tsx
@@ -1,9 +1,13 @@
-import gql from 'graphql-tag';
-import * as Urql from 'urql';
+import gql from "graphql-tag";
+import * as Urql from "urql";
 export type Maybe<T> = T | null;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
+  { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
+  { [SubKey in K]: Maybe<T[SubKey]> };
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
@@ -15,207 +19,219 @@ export type Scalars = {
 };
 
 export type Mutation = {
-  __typename?: 'Mutation';
+  __typename?: "Mutation";
   createPost: Post;
   updatePost: Post;
-  deletePost: Scalars['Boolean'];
+  deletePost: Scalars["Boolean"];
   register: UserRes;
   login: UserRes;
-  logout: Scalars['Boolean'];
+  logout: Scalars["Boolean"];
 };
-
 
 export type MutationCreatePostArgs = {
-  title: Scalars['String'];
+  title: Scalars["String"];
 };
-
 
 export type MutationUpdatePostArgs = {
-  title?: Maybe<Scalars['String']>;
-  id: Scalars['Float'];
+  title?: Maybe<Scalars["String"]>;
+  id: Scalars["Float"];
 };
-
 
 export type MutationDeletePostArgs = {
-  id: Scalars['Float'];
+  id: Scalars["Float"];
 };
-
 
 export type MutationRegisterArgs = {
   options: UserPassInput;
 };
-
 
 export type MutationLoginArgs = {
   options: UserPassInput;
 };
 
 export type Post = {
-  __typename?: 'Post';
-  _id: Scalars['Float'];
-  createdAt: Scalars['String'];
-  updatedAt: Scalars['String'];
-  title: Scalars['String'];
+  __typename?: "Post";
+  _id: Scalars["Float"];
+  createdAt: Scalars["String"];
+  updatedAt: Scalars["String"];
+  title: Scalars["String"];
 };
 
 export type Query = {
-  __typename?: 'Query';
-  hello: Scalars['String'];
+  __typename?: "Query";
+  hello: Scalars["String"];
   posts: Array<Post>;
   post?: Maybe<Post>;
   me?: Maybe<User>;
 };
 
-
 export type QueryPostArgs = {
-  id: Scalars['Int'];
+  id: Scalars["Int"];
 };
 
 export type User = {
-  __typename?: 'User';
-  _id: Scalars['Float'];
-  createdAt: Scalars['String'];
-  updatedAt: Scalars['String'];
-  username: Scalars['String'];
+  __typename?: "User";
+  _id: Scalars["Float"];
+  createdAt: Scalars["String"];
+  updatedAt: Scalars["String"];
+  username: Scalars["String"];
 };
 
 export type UserError = {
-  __typename?: 'UserError';
-  field: Scalars['String'];
-  message: Scalars['String'];
+  __typename?: "UserError";
+  field: Scalars["String"];
+  message: Scalars["String"];
 };
 
 export type UserPassInput = {
-  username: Scalars['String'];
-  password: Scalars['String'];
+  username: Scalars["String"];
+  password: Scalars["String"];
 };
 
 export type UserRes = {
-  __typename?: 'UserRes';
+  __typename?: "UserRes";
   errors?: Maybe<Array<UserError>>;
   user?: Maybe<User>;
 };
 
-export type RegUserFragment = (
-  { __typename?: 'User' }
-  & Pick<User, '_id' | 'username'>
-);
+export type RegUserFragment = { __typename?: "User" } & Pick<
+  User,
+  "_id" | "username"
+>;
 
 export type LoginMutationVariables = Exact<{
   options: UserPassInput;
 }>;
 
+export type LoginMutation = { __typename?: "Mutation" } & {
+  login: { __typename?: "UserRes" } & {
+    errors?: Maybe<
+      Array<{ __typename?: "UserError" } & Pick<UserError, "field" | "message">>
+    >;
+    user?: Maybe<{ __typename?: "User" } & RegUserFragment>;
+  };
+};
 
-export type LoginMutation = (
-  { __typename?: 'Mutation' }
-  & { login: (
-    { __typename?: 'UserRes' }
-    & { errors?: Maybe<Array<(
-      { __typename?: 'UserError' }
-      & Pick<UserError, 'field' | 'message'>
-    )>>, user?: Maybe<(
-      { __typename?: 'User' }
-      & RegUserFragment
-    )> }
-  ) }
-);
+export type LogoutMutationVariables = Exact<{ [key: string]: never }>;
 
-export type LogoutMutationVariables = Exact<{ [key: string]: never; }>;
-
-
-export type LogoutMutation = (
-  { __typename?: 'Mutation' }
-  & Pick<Mutation, 'logout'>
-);
+export type LogoutMutation = { __typename?: "Mutation" } & Pick<
+  Mutation,
+  "logout"
+>;
 
 export type RegisterMutationVariables = Exact<{
-  username: Scalars['String'];
-  password: Scalars['String'];
+  username: Scalars["String"];
+  password: Scalars["String"];
 }>;
 
+export type RegisterMutation = { __typename?: "Mutation" } & {
+  register: { __typename?: "UserRes" } & {
+    errors?: Maybe<
+      Array<{ __typename?: "UserError" } & Pick<UserError, "field" | "message">>
+    >;
+    user?: Maybe<{ __typename?: "User" } & RegUserFragment>;
+  };
+};
 
-export type RegisterMutation = (
-  { __typename?: 'Mutation' }
-  & { register: (
-    { __typename?: 'UserRes' }
-    & { errors?: Maybe<Array<(
-      { __typename?: 'UserError' }
-      & Pick<UserError, 'field' | 'message'>
-    )>>, user?: Maybe<(
-      { __typename?: 'User' }
-      & RegUserFragment
-    )> }
-  ) }
-);
+export type MeQueryVariables = Exact<{ [key: string]: never }>;
 
-export type MeQueryVariables = Exact<{ [key: string]: never; }>;
+export type MeQuery = { __typename?: "Query" } & {
+  me?: Maybe<{ __typename?: "User" } & RegUserFragment>;
+};
 
+export type PostsQueryVariables = Exact<{ [key: string]: never }>;
 
-export type MeQuery = (
-  { __typename?: 'Query' }
-  & { me?: Maybe<(
-    { __typename?: 'User' }
-    & RegUserFragment
-  )> }
-);
+export type PostsQuery = { __typename?: "Query" } & {
+  posts: Array<
+    { __typename?: "Post" } & Pick<
+      Post,
+      "title" | "_id" | "createdAt" | "updatedAt"
+    >
+  >;
+};
 
 export const RegUserFragmentDoc = gql`
-    fragment RegUser on User {
-  _id
-  username
-}
-    `;
+  fragment RegUser on User {
+    _id
+    username
+  }
+`;
 export const LoginDocument = gql`
-    mutation Login($options: UserPassInput!) {
-  login(options: $options) {
-    errors {
-      field
-      message
-    }
-    user {
-      ...RegUser
+  mutation Login($options: UserPassInput!) {
+    login(options: $options) {
+      errors {
+        field
+        message
+      }
+      user {
+        ...RegUser
+      }
     }
   }
-}
-    ${RegUserFragmentDoc}`;
+  ${RegUserFragmentDoc}
+`;
 
 export function useLoginMutation() {
   return Urql.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument);
-};
-export const LogoutDocument = gql`
-    mutation Logout {
-  logout
 }
-    `;
+export const LogoutDocument = gql`
+  mutation Logout {
+    logout
+  }
+`;
 
 export function useLogoutMutation() {
-  return Urql.useMutation<LogoutMutation, LogoutMutationVariables>(LogoutDocument);
-};
+  return Urql.useMutation<LogoutMutation, LogoutMutationVariables>(
+    LogoutDocument
+  );
+}
 export const RegisterDocument = gql`
-    mutation Register($username: String!, $password: String!) {
-  register(options: {username: $username, password: $password}) {
-    errors {
-      field
-      message
+  mutation Register($username: String!, $password: String!) {
+    register(options: { username: $username, password: $password }) {
+      errors {
+        field
+        message
+      }
+      user {
+        ...RegUser
+      }
     }
-    user {
+  }
+  ${RegUserFragmentDoc}
+`;
+
+export function useRegisterMutation() {
+  return Urql.useMutation<RegisterMutation, RegisterMutationVariables>(
+    RegisterDocument
+  );
+}
+export const MeDocument = gql`
+  query Me {
+    me {
       ...RegUser
     }
   }
-}
-    ${RegUserFragmentDoc}`;
+  ${RegUserFragmentDoc}
+`;
 
-export function useRegisterMutation() {
-  return Urql.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument);
-};
-export const MeDocument = gql`
-    query Me {
-  me {
-    ...RegUser
-  }
-}
-    ${RegUserFragmentDoc}`;
-
-export function useMeQuery(options: Omit<Urql.UseQueryArgs<MeQueryVariables>, 'query'> = {}) {
+export function useMeQuery(
+  options: Omit<Urql.UseQueryArgs<MeQueryVariables>, "query"> = {}
+) {
   return Urql.useQuery<MeQuery>({ query: MeDocument, ...options });
-};
+}
+export const PostsDocument = gql`
+  query Posts {
+    posts {
+      title
+      _id
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
+export function usePostsQuery(
+  options: Omit<Urql.UseQueryArgs<PostsQueryVariables>, "query"> = {}
+) {
+  return Urql.useQuery<PostsQuery>({ query: PostsDocument, ...options });
+}
--- r_frontend/src/pages/_app.tsx
+++ r_frontend/src/pages/_app.tsx
@@ -1,90 +1,19 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
-import { cacheExchange, Cache, QueryInput } from "@urql/exchange-graphcache";
-import { Provider, createClient, dedupExchange, fetchExchange } from "urql";
-import {
-  LoginMutation,
-  LogoutMutation,
-  MeDocument,
-  MeQuery,
-  RegisterMutation,
-} from "../generated/graphql";
 import theme from "../theme";
 
-function bUpQuery<Result, Query>(
-  cache: Cache,
-  qi: QueryInput,
-  result: any,
-  fn: (r: Result, q: Query) => Query
-) {
-  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
-}
-
-const client = createClient({
-  url: "http://localhost:4000/graphql",
-  fetchOptions: {
-    credentials: "include",
-  },
-  exchanges: [
-    dedupExchange,
-    cacheExchange({
-      updates: {
-        Mutation: {
-          logout: (_result, args, cache, info) => {
-            bUpQuery<LogoutMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              () => ({ me: null })
-            );
-          },
-          login: (_result, args, cache, info) => {
-            bUpQuery<LoginMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              (result, query) => {
-                if (result.login.errors) {
-                  return qeury;
-                } else {
-                  return { me: result.login.user };
-                }
-              }
-            );
-          },
-          register: (_result, args, cache, info) => {
-            bUpQuery<RegisterMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              (result, query) => {
-                if (result.register.errors) {
-                  return qeury;
-                } else {
-                  return { me: result.register.user };
-                }
-              }
-            );
-          },
-        },
-      },
-    }),
-    fetchExchange,
-  ],
-});
+// const client = createClient({});
 
 function MyApp({ Component, pageProps }: any) {
   return (
-    <Provider value={client}>
-      <ChakraProvider resetCSS theme={theme}>
-        <ColorModeProvider
-          options={{
-            useSystemColorMode: true,
-          }}
-        >
-          <Component {...pageProps} />
-        </ColorModeProvider>
-      </ChakraProvider>
-    </Provider>
+    <ChakraProvider resetCSS theme={theme}>
+      <ColorModeProvider
+        options={{
+          useSystemColorMode: true,
+        }}
+      >
+        <Component {...pageProps} />
+      </ColorModeProvider>
+    </ChakraProvider>
   );
 }
 
