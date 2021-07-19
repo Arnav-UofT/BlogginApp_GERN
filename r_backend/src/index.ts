e60fdb2b4919a9655ebd60befee096f7540b1ea0
arnavgupta <arnavg.gupta@mail.utoronto.ca>
2021-07-20 00:57:59 +0530

    a

--- r_backend/src/index.ts
+++ r_backend/src/index.ts
@@ -10,7 +10,7 @@ import { UserResolver } from "./resolvers/user";
 import Redis from "ioredis";
 import session from "express-session";
 import connectRedis from "connect-redis";
-// import cors from "cors";
+import cors from "cors";
 import { createConnection } from "typeorm";
 import { User } from "./entities/User";
 import path from "path";
@@ -43,12 +43,7 @@ const main = async () => {
     host: process.env.REDIS_HOST || "localhost",
   });
 
-  // app.use(
-  //   cors({
-  //     origin: process.env.ORIGIN || "http://localhost:3000",
-  //     credentials: true,
-  //   })
-  // );
+  app.use(cors());
 
   app.use(
     session({
