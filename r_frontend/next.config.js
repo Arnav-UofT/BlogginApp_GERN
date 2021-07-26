748b227f039fc727ce5be1078c03c55536c5c458
arnavgupta <arnavg.gupta@mail.utoronto.ca>
2021-07-26 14:00:08 +0530

    n

--- r_frontend/next.config.js
+++ r_frontend/next.config.js
@@ -1,5 +1,13 @@
 module.exports = {
     future: {
         webpack5: true,
-    }
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/:path*',
+                destination: 'http://128.199.26.89:4000/graphql/:path*',
+            },
+        ]
+    },
 }
